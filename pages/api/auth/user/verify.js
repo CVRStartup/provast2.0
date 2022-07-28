11fa0615f80a3ef3603b2a61843dc1b81831eaaa
Thusharke <thushar.ke25@gmail.com>
2022-07-28 16:43:43 +0530

    added student

---
+++ pages/api/auth/user/verify.js
@@ -0,0 +1,43 @@
+import connectDB from "../../../../src/lib/connectDB";
+import User from "../../../../models/User.js";
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case "GET":
+      await verifyPassphrase(req, res);
+      break;
+  }
+}
+
+const verifyPassphrase = async (req, res) => {
+  try {
+    await connectDB();
+
+    const { pass } = req.query;
+
+    console.log(req.body);
+
+    if (!pass) {
+      return res.status(400).json({ message: "Invalid Passphrase" });
+    }
+
+    const college = await User.findOne({ "college.passphrase": pass });
+
+    if (college.approved) {
+      return res.status(200).json({
+        message: "College found",
+        verified: true,
+        college: {
+          name: college.college.name,
+          code: college.college.code,
+        },
+      });
+    } else {
+      return res
+        .status(200)
+        .json({ message: "Please try again!", verified: false, college: false });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
