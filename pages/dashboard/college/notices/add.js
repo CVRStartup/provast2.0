b8376493d0c43b5b063a2f917aa2d83a24d34c3a
Thusharke <thushar.ke25@gmail.com>
2022-08-10 15:59:09 +0530

    linked apui

--- pages/dashboard/college/notices/add.js
+++ pages/dashboard/college/notices/add.js
@@ -41,11 +41,13 @@ const JobAdd = ({ user }) => {
             const worksheetName = workbook.SheetNames[0];
             const worksheet = workbook.Sheets[worksheetName];
             const data = XLSX.utils.sheet_to_json(worksheet);
-            const res = data.forEach((x) => {
+            const res = data.map((x) => {
               return {
                 email: x["Email"] ? x["Email"] : "N/A",
               };
             });
+            console.log(res);
+            console.log(data);
             setEligible(res);
           } else {
             setEligible([]);
@@ -63,10 +65,9 @@ const JobAdd = ({ user }) => {
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     console.log(eligible);
-
     const {
       data: { message },
-    } = await axios.post(`${process.env.NEXT_PUBLIC_HOST_URL}/api/jobs`, {
+    } = await axios.post(`${process.env.NEXT_PUBLIC_HOST_URL}/api/notices`, {
       user: user._id,
       author: {
         name: user.profile.firstName + " " + user.profile.lastName,
@@ -79,63 +80,63 @@ const JobAdd = ({ user }) => {
       title: title,
       description: description,
       status: selectedStatus.name,
-      visible: visible,
+      visible: eligible,
     });
 
-    if (message == "Success! Job Created") {
+    if (message == "Success! Notice Created") {
       toast.success(message, { toastId: message });
-      router.push("/dashboard/college/jobs");
+      router.push("/dashboard/college/notices");
     } else {
       toast.error(message, { toastId: message });
     }
   };
 
   return (
-    <main className='bg-gray-50 pt-[10vh]'>
+    <main className="bg-gray-50 h-screen pt-[15vh]">
       {/* {loading.type === "add" && loading.status === true ? <Loading /> : ""} */}
-      <div className='space-y-6 max-w-6xl mx-auto py-8'>
-        <div className='bg-white shadow px-4 py-5 sm:rounded-lg sm:p-6'>
-          <div className='mb-5 md:col-span-1'>
-            <h3 className='text-lg font-medium leading-6 text-gray-900'>Job Infomation</h3>
-            <p className='mt-1 text-sm text-gray-500'>
+      <div className="space-y-6 max-w-6xl mx-auto py-8">
+        <div className="bg-white shadow px-4 py-5 sm:rounded-lg sm:p-6">
+          <div className="mb-5 md:col-span-1">
+            <h3 className="text-lg font-medium leading-6 text-gray-900">Create Notice</h3>
+            <p className="mt-1 text-sm text-gray-500">
               This information will be displayed publicly so be careful what you share.
             </p>
           </div>
           <div>
-            <form className='mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6' method='POST'>
-              <div className='sm:col-span-6'>
-                <label htmlFor='name' className='block text-sm font-medium text-gray-700'>
+            <form className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6" method="POST">
+              <div className="sm:col-span-6">
+                <label htmlFor="name" className="block text-sm font-medium text-gray-700">
                   Title
                 </label>
                 <input
-                  type='text'
-                  name='name'
-                  id='name'
+                  type="text"
+                  name="name"
+                  id="name"
                   value={title}
                   onChange={(e) => setTitle(e.target.value)}
-                  autoComplete='off'
-                  className='mt-1 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md'
+                  autoComplete="off"
+                  className="mt-1 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                 />
               </div>
 
-              <div className='sm:col-span-6'>
-                <label htmlFor='purpose' className='block text-sm font-medium text-gray-700'>
+              <div className="sm:col-span-6">
+                <label htmlFor="purpose" className="block text-sm font-medium text-gray-700">
                   Description
                 </label>
                 <Editor input={description} dataCallBack={handleCallBack} />
-                <p className='mt-2 text-sm text-gray-500'>Few lines to describe the job role.</p>
+                <p className="mt-2 text-sm text-gray-500">Few lines to describe the job role.</p>
               </div>
-              <div className='sm:col-span-3'>
-                <label htmlFor='photo' className='block text-sm font-medium text-gray-700'>
+              <div className="sm:col-span-3">
+                <label htmlFor="photo" className="block text-sm font-medium text-gray-700">
                   Upload Spreadsheet
                 </label>
 
                 <input
-                  className='mt-2 appearance-none block w-full p-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm'
-                  label='Choose File'
-                  type='file'
-                  name='image'
-                  id='profileImg'
+                  className="mt-2 appearance-none block w-full p-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
+                  label="Choose File"
+                  type="file"
+                  name="image"
+                  id="profileImg"
                   onChange={handleFile}
                 />
                 {excelFileError &&
@@ -143,7 +144,7 @@ const JobAdd = ({ user }) => {
                     toastId: excelFileError,
                   })}
               </div>
-              <div className='sm:col-span-3 relative -top-[22px]'>
+              <div className="sm:col-span-3 relative -top-[22px]">
                 <DropDown
                   title={"Status"}
                   options={status}
@@ -152,11 +153,11 @@ const JobAdd = ({ user }) => {
                 />
               </div>
             </form>
-            <div className='flex justify-end mt-6'>
+            <div className="flex justify-end mt-6">
               <Link href={`/dashboard/college/jobs`}>
                 <button
-                  type='button'
-                  className='bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500'
+                  type="button"
+                  className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
                 >
                   Cancel
                 </button>
