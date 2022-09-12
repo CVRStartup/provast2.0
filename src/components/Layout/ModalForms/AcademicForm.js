1c15331c19895f5d1f1c33cee9fee9ab61a118d1
adilshaik <shaik.adil@hotmail.com>
2022-09-12 22:58:05 +0530

    Added mutate

--- src/components/Layout/ModalForms/AcademicForm.js
+++ src/components/Layout/ModalForms/AcademicForm.js
@@ -15,8 +15,6 @@ import { useUser } from "../../../lib/hooks";
 import { DropDown } from "../../Reusables/Dropdown";
 import { mutate } from "swr";
 import axios from "axios";
-import { toast } from "react-toastify";
-import { useAcademicDetails } from "../../../hooks/useAcademicDetails";
 
 export const AcademicForm = () => {
   const session = useUser();
@@ -100,29 +98,30 @@ export const AcademicForm = () => {
         },
       });
     }
+    await mutate(`/api/auth/user/academics?rollNumber=${editRollNumber}`);
     closeModal();
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="flex items-center justify-between">
-        <Dialog.Title as="h3" className="text-2xl font-medium leading-6 text-white">
+      <div className='flex items-center justify-between'>
+        <Dialog.Title as='h3' className='text-2xl font-medium leading-6 text-white'>
           {isEdit ? "Edit " : "Add "}Education
         </Dialog.Title>
       </div>
-      <div className="mt-5 w-full">
+      <div className='mt-5 w-full'>
         <React.Fragment>
-          <div className="col-span-6 sm:col-span-6 mt-2">
-            <div className="flex">
-              <label htmlFor="school" className="block text-sm font-medium text-white">
+          <div className='col-span-6 sm:col-span-6 mt-2'>
+            <div className='flex'>
+              <label htmlFor='school' className='block text-sm font-medium text-white'>
                 School / Institution
               </label>
-              <span className="ml-1 text-red-600 font-semibold">*</span>
+              <span className='ml-1 text-red-600 font-semibold'>*</span>
             </div>
             <input
-              type="text"
-              name="school"
-              id="school"
+              type='text'
+              name='school'
+              id='school'
               value={academics.institution}
               onChange={(e) =>
                 setAcademics({
@@ -131,11 +130,11 @@ export const AcademicForm = () => {
                 })
               }
               required
-              className="mt-1 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              className='mt-1 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md'
             />
           </div>
 
-          <div className="grid grid-cols-6 gap-4">
+          <div className='grid grid-cols-6 gap-4'>
             <div
               className={`col-span-6 ${
                 !(selectedDegree.name === "Class Xth" || selectedDegree.name === "Class XIIth")
@@ -153,7 +152,7 @@ export const AcademicForm = () => {
               />
             </div>
             {!(selectedDegree.name === "Class Xth" || selectedDegree.name === "Class XIIth") && (
-              <div className="col-span-6 sm:col-span-3">
+              <div className='col-span-6 sm:col-span-3'>
                 <DropDown
                   title={"Branch / Specialization"}
                   isRequired
@@ -173,25 +172,25 @@ export const AcademicForm = () => {
               </div>
             )}
           </div>
-          <div className="grid grid-cols-6 gap-4 mt-10">
-            <div className="col-span-6 sm:col-span-3">
-              <div className="flex">
-                <label htmlFor="board" className="block text-sm font-medium text-white">
+          <div className='grid grid-cols-6 gap-4 mt-10'>
+            <div className='col-span-6 sm:col-span-3'>
+              <div className='flex'>
+                <label htmlFor='board' className='block text-sm font-medium text-white'>
                   Board / University
                 </label>
-                <span className="ml-1 text-red-600 font-semibold">*</span>
+                <span className='ml-1 text-red-600 font-semibold'>*</span>
               </div>
               <input
-                type="text"
-                name="board"
-                id="board"
+                type='text'
+                name='board'
+                id='board'
                 required
                 value={academics.board}
                 onChange={(e) => setAcademics({ ...academics, board: e.target.value })}
-                className="mt-1 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                className='mt-1 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md'
               />
             </div>
-            <div className="col-span-6 sm:col-span-3 relative -top-[23px]">
+            <div className='col-span-6 sm:col-span-3 relative -top-[23px]'>
               <DropDown
                 title={"Education Type"}
                 isRequired
@@ -203,18 +202,18 @@ export const AcademicForm = () => {
             </div>
           </div>
 
-          <div className="flex mt-4">
-            <label htmlFor="score" className="block text-sm font-medium text-white">
+          <div className='flex mt-4'>
+            <label htmlFor='score' className='block text-sm font-medium text-white'>
               Score
             </label>
-            <span className="ml-1 text-red-600 font-semibold">*</span>
+            <span className='ml-1 text-red-600 font-semibold'>*</span>
           </div>
-          <div className="grid grid-cols-6 gap-4">
-            <div className="col-span-6 sm:col-span-3 ">
+          <div className='grid grid-cols-6 gap-4'>
+            <div className='col-span-6 sm:col-span-3 '>
               <input
-                type="text"
-                name="score"
-                id="score"
+                type='text'
+                name='score'
+                id='score'
                 required
                 value={academics.score.grade}
                 onChange={(e) =>
@@ -226,10 +225,10 @@ export const AcademicForm = () => {
                     },
                   })
                 }
-                className="mt-1 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                className='mt-1 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md'
               />
             </div>
-            <div className="col-span-6 sm:col-span-3 relative -top-[45px]">
+            <div className='col-span-6 sm:col-span-3 relative -top-[45px]'>
               <DropDown
                 title={"Grade"}
                 isRequired
@@ -241,21 +240,21 @@ export const AcademicForm = () => {
             </div>
           </div>
 
-          <div className="flex">
-            <label htmlFor="duration" className="block text-sm font-medium text-white">
+          <div className='flex'>
+            <label htmlFor='duration' className='block text-sm font-medium text-white'>
               From
             </label>
-            <span className="ml-1 text-red-600 font-semibold">*</span>
+            <span className='ml-1 text-red-600 font-semibold'>*</span>
           </div>
-          <div className="grid grid-cols-6 gap-6">
-            <div className="col-span-6 sm:col-span-3">
+          <div className='grid grid-cols-6 gap-6'>
+            <div className='col-span-6 sm:col-span-3'>
               <input
-                type="number"
-                name="duration"
-                placeholder="YYYY"
-                min="2001"
-                max="2100"
-                id="duration"
+                type='number'
+                name='duration'
+                placeholder='YYYY'
+                min='2001'
+                max='2100'
+                id='duration'
                 value={academics.batch.from}
                 onChange={(e) =>
                   setAcademics({
@@ -264,18 +263,18 @@ export const AcademicForm = () => {
                   })
                 }
                 required
-                className="mt-1 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                className='mt-1 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md'
               />
             </div>
 
-            <div className="col-span-6 sm:col-span-3">
+            <div className='col-span-6 sm:col-span-3'>
               <input
-                type="number"
-                placeholder="YYYY"
-                min="2001"
-                max="2100"
-                name="duration"
-                id="duration"
+                type='number'
+                placeholder='YYYY'
+                min='2001'
+                max='2100'
+                name='duration'
+                id='duration'
                 value={academics.batch.to}
                 onChange={(e) =>
                   setAcademics({
@@ -283,24 +282,24 @@ export const AcademicForm = () => {
                     batch: { ...academics.batch, to: parseInt(e.target.value) },
                   })
                 }
-                className="mt-1 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                className='mt-1 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md'
               />
             </div>
           </div>
         </React.Fragment>
       </div>
-      <div className="pt-5">
-        <div className="flex justify-end">
+      <div className='pt-5'>
+        <div className='flex justify-end'>
           <button
-            type="button"
+            type='button'
             onClick={closeModal}
-            className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+            className='bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500'
           >
             Cancel
           </button>
           <button
-            type="submit"
-            className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+            type='submit'
+            className='ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500'
           >
             {session.category === "college" ? "Freeze" : "Save"}
           </button>
