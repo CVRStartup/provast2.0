108628cf980b3a823b800a020d5d3cc6a2436934
Thusharke <thushar.ke25@gmail.com>
2022-10-12 19:42:41 +0530

    fixed resume bugs

--- src/components/Resumes/Templates/Onyx.js
+++ src/components/Resumes/Templates/Onyx.js
@@ -19,44 +19,44 @@ export const Onyx = ({ componentRef, filter = null }) => {
     layout,
   } = useResumeContext();
   return (
-    <div ref={componentRef} className='flex w-a4W bg-white mx-auto h-a4H my-5'>
+    <div ref={componentRef} className="flex w-a4W bg-white mx-auto h-a4H my-5">
       <div
-        className='flex flex-col items-center h-full w-64 px-3 py-2'
+        className="flex flex-col items-center h-full w-64 px-3 py-2"
         style={{ backgroundColor: `rgba(${0}, ${0}, ${0}, 0.05)` }}
       >
-        <h2 className='text-3xl font-bold text-gray-700 capitalize tracking-wide mt-2'>
+        <h2 className="text-3xl font-bold text-gray-700 capitalize tracking-wide mt-2">
           {rename(profile?.firstName)} {rename(profile?.lastName)}
         </h2>
-        <h1 className='mt-2 text-xs uppercase font-bold text-gray-500 tracking-wider'>
+        <h1 className="mt-2 text-xs uppercase font-bold text-gray-500 tracking-wider">
           {profile?.role}
         </h1>
-        <div className='mt-10 w-full'>
-          <h3 className='text-[18px] text-gray-500 uppercase font-extrabold tracking-wide border-b-2 border-gray-300'>
+        <div className="mt-10 w-full">
+          <h3 className="text-[18px] text-gray-500 uppercase font-extrabold tracking-wide border-b-2 border-gray-300">
             Personal Info
           </h3>
-          <div className='flex justify-between items-center mt-3'>
-            <h6 className='text-xs font-bold text-gray-600'>Email</h6>
+          <div className="flex justify-between items-center mt-3">
+            <h6 className="text-xs font-bold text-gray-600">Email</h6>
             <h6 className={`text-xs ${filter?.removeEmail ? "blur" : ""} `}>{profile?.email}</h6>
           </div>
-          <div className='flex justify-between items-center mt-3'>
-            <h6 className='text-xs font-bold text-gray-600'>Phone Number</h6>
+          <div className="flex justify-between items-center mt-3">
+            <h6 className="text-xs font-bold text-gray-600">Phone Number</h6>
             <h6 className={`text-xs ${filter?.removePhoneNumber ? "blur" : ""}`}>
               {profile?.phone}
             </h6>
           </div>
-          <div className='flex justify-between items-center mt-3'>
-            <h6 className='text-xs font-bold text-gray-600'>Date Of Birth</h6>
-            <h6 className='text-xs'>{profile?.dob}</h6>
+          <div className="flex justify-between items-center mt-3">
+            <h6 className="text-xs font-bold text-gray-600">Date Of Birth</h6>
+            <h6 className="text-xs">{profile?.dob}</h6>
           </div>
 
           {social?.filter((option) => option?.enabled)?.length > 0 && (
-            <div className='mb-2'>
+            <div className="mb-2">
               {social?.map((option) => {
                 if (option?.enabled === true) {
                   return (
-                    <div key={option?._id} className='flex justify-between items-center mt-3'>
-                      <h6 className='text-xs font-bold text-gray-600'>{option?.network}</h6>
-                      <a href={option?.url} className='text-xs'>
+                    <div key={option?._id} className="flex justify-between items-center mt-3">
+                      <h6 className="text-xs font-bold text-gray-600">{option?.network}</h6>
+                      <a href={option?.url} className="text-xs">
                         {option?.username}
                       </a>
                     </div>
@@ -68,8 +68,8 @@ export const Onyx = ({ componentRef, filter = null }) => {
         </div>
 
         {certifications?.filter((certification) => certification?.enabled)?.length > 0 && (
-          <div className='mt-5 w-full'>
-            <h3 className='text-[18px] text-gray-500 uppercase font-extrabold tracking-wide border-b-2 border-gray-300'>
+          <div className="mt-5 w-full">
+            <h3 className="text-[18px] text-gray-500 uppercase font-extrabold tracking-wide border-b-2 border-gray-300">
               Certifications
             </h3>
 
@@ -79,13 +79,13 @@ export const Onyx = ({ componentRef, filter = null }) => {
                   return (
                     <div
                       key={certification?._id}
-                      className='flex justify-between items-center mt-2 mx-1 w-56'
+                      className="flex justify-between items-center mt-2 mx-1 w-56"
                     >
                       <div>
-                        <h6 className='text-xs font-bold'>{certification?.title}</h6>
-                        <h6 className='text-xs'>{certification?.issuer}</h6>
+                        <h6 className="text-xs font-bold">{certification?.title}</h6>
+                        <h6 className="text-xs">{certification?.issuer}</h6>
                       </div>
-                      <h6 className='text-xs'>{new Date(certification?.date).getFullYear()}</h6>
+                      <h6 className="text-xs">{new Date(certification?.date).getFullYear()}</h6>
                     </div>
                   );
                 }
@@ -95,8 +95,8 @@ export const Onyx = ({ componentRef, filter = null }) => {
         )}
 
         {work?.filter((option) => option?.enabled)?.length > 0 && (
-          <div className='mt-5'>
-            <h3 className='text-[18px] text-gray-500 uppercase font-extrabold tracking-wide border-b-2 border-gray-300'>
+          <div className="mt-5">
+            <h3 className="text-[18px] text-gray-500 uppercase font-extrabold tracking-wide border-b-2 border-gray-300">
               Work Experience
             </h3>
 
@@ -104,13 +104,13 @@ export const Onyx = ({ componentRef, filter = null }) => {
               {work.map((option) => {
                 if (option?.enabled === true) {
                   return (
-                    <div key={option?._id} className='flex justify-between mt-2 mx-1 w-56'>
+                    <div key={option?._id} className="flex justify-between mt-2 mx-1 w-56">
                       <div>
-                        <h6 className='text-xs font-bold'>{option?.company}</h6>
-                        <h6 className='text-xs'>{option?.designation}</h6>
+                        <h6 className="text-xs font-bold">{option?.company}</h6>
+                        <h6 className="text-xs">{option?.designation}</h6>
                       </div>
-                      <h6 className='text-xs'>
-                        {new Date(new Date(option?.from) - new Date(option?.to)).getMonth()} Months
+                      <h6 className="text-xs">
+                        {new Date(option?.to).getMonth() - new Date(option?.from).getMonth()} Months
                       </h6>
                     </div>
                   );
@@ -121,18 +121,18 @@ export const Onyx = ({ componentRef, filter = null }) => {
         )}
 
         {skills?.filter((skill) => skill?.enabled)?.length > 0 && (
-          <div className='w-full mt-5'>
-            <h3 className='text-[18px] text-gray-500 uppercase font-extrabold tracking-wide border-b-2 border-gray-300'>
+          <div className="w-full mt-5">
+            <h3 className="text-[18px] text-gray-500 uppercase font-extrabold tracking-wide border-b-2 border-gray-300">
               Skills
             </h3>
 
             {skills?.map((skill) => {
               if (skill?.enabled === true) {
                 return (
-                  <div key={skill?._id} className='mt-2'>
-                    <div className='mb-1 flex justify-between'>
-                      <h6 className='text-sm font-bold capitalize'>{skill?.name}</h6>
-                      <span className='capitalize text-xs'>{skill?.level}</span>
+                  <div key={skill?._id} className="mt-2">
+                    <div className="mb-1 flex justify-between">
+                      <h6 className="text-sm font-bold capitalize">{skill?.name}</h6>
+                      <span className="capitalize text-xs">{skill?.level}</span>
                     </div>
                   </div>
                 );
@@ -142,8 +142,8 @@ export const Onyx = ({ componentRef, filter = null }) => {
         )}
 
         {languages?.filter((language) => language?.enabled)?.length > 0 && (
-          <div className='w-full mt-5'>
-            <h3 className='text-[18px] text-gray-500 uppercase font-extrabold tracking-wide border-b-2 border-gray-300'>
+          <div className="w-full mt-5">
+            <h3 className="text-[18px] text-gray-500 uppercase font-extrabold tracking-wide border-b-2 border-gray-300">
               Languages
             </h3>
             <div>
@@ -152,10 +152,10 @@ export const Onyx = ({ componentRef, filter = null }) => {
                   return (
                     <div
                       key={language?._id}
-                      className='flex justify-between items-center mt-2 mx-1 w-56'
+                      className="flex justify-between items-center mt-2 mx-1 w-56"
                     >
-                      <h6 className='text-xs font-bold'>{language?.name}</h6>
-                      <h6 className='text-xs'>{language?.fluency}</h6>
+                      <h6 className="text-xs font-bold">{language?.name}</h6>
+                      <h6 className="text-xs">{language?.fluency}</h6>
                     </div>
                   );
                 }
@@ -164,29 +164,29 @@ export const Onyx = ({ componentRef, filter = null }) => {
           </div>
         )}
       </div>
-      <div className='w-11/12 mx-4 py-4'>
+      <div className="w-11/12 mx-4 py-4">
         {objective && (
-          <div className='px-2'>
-            <h3 className='text-[20px] text-gray-500 uppercase font-extrabold tracking-wider'>
+          <div className="px-2">
+            <h3 className="text-[20px] text-gray-500 uppercase font-extrabold tracking-wider">
               Objective
             </h3>
-            <div className='border-b-2 mb-2 w-full mx-auto border-gray-300'></div>
-            <h4 className='markdown text-[13.5px] tracking-wide font-semibold text-gray-700'>
+            <div className="border-b-2 mb-2 w-full mx-auto border-gray-300"></div>
+            <h4 className="markdown text-[13.5px] tracking-wide font-semibold text-gray-700">
               <MarkdownRenderer>{objective}</MarkdownRenderer>
             </h4>
           </div>
         )}
         {education?.filter((option) => option?.enabled)?.length > 0 && (
-          <div className='mt-5 w-full'>
-            <h3 className='text-[20px] text-gray-500 uppercase font-extrabold tracking-wider'>
+          <div className="mt-5 w-full">
+            <h3 className="text-[20px] text-gray-500 uppercase font-extrabold tracking-wider">
               Education
             </h3>
-            <div className='border-b-2 mb-2 w-full mx-auto border-gray-300'></div>
+            <div className="border-b-2 mb-2 w-full mx-auto border-gray-300"></div>
             {education
               ?.filter((option) => option?.enabled === true)
               .map((option) => (
-                <div key={option?._id} className='flex justify-between items-start mb-3'>
-                  <div className='leading-4'>
+                <div key={option?._id} className="flex justify-between items-start mb-3">
+                  <div className="leading-4">
                     <h3
                       className={`${
                         option?.typeOfDegree !== "School / Intermediate" &&
@@ -197,16 +197,16 @@ export const Onyx = ({ componentRef, filter = null }) => {
                     >
                       {option?.institution}
                     </h3>
-                    <span className='text-[13px]'>
+                    <span className="text-[13px]">
                       {option?.fieldOfStudy} &middot; {option?.gpa}
                     </span>
                     {option?.summary?.enabled && (
-                      <h4 className='markdown text-[13px] tracking-wide font-semibold text-gray-700 mt-1'>
+                      <h4 className="markdown text-[13px] tracking-wide font-semibold text-gray-700 mt-1">
                         <MarkdownRenderer>{option?.summary?.data}</MarkdownRenderer>
                       </h4>
                     )}
                   </div>
-                  <span className='text-xs font-bold tracking-wide'>
+                  <span className="text-xs font-bold tracking-wide">
                     {new Date(option?.startDate).getFullYear()} -{" "}
                     {new Date(option?.endDate).getFullYear()}
                   </span>
@@ -215,33 +215,33 @@ export const Onyx = ({ componentRef, filter = null }) => {
           </div>
         )}
         {projects?.filter((project) => project?.enabled)?.length > 0 && (
-          <div className='mt-5 w-full'>
-            <h3 className='text-[20px] text-gray-500 uppercase font-extrabold tracking-wider'>
+          <div className="mt-5 w-full">
+            <h3 className="text-[20px] text-gray-500 uppercase font-extrabold tracking-wider">
               Projects
             </h3>
-            <div className='border-b-2 mb-2 w-full mx-auto border-gray-300'></div>
+            <div className="border-b-2 mb-2 w-full mx-auto border-gray-300"></div>
 
-            <div className='mt-2'>
+            <div className="mt-2">
               {projects?.map((project) => {
                 if (project?.enabled === true) {
                   return (
-                    <div key={project?._id} className='mb-2'>
-                      <div className='flex flex-col justify-between'>
-                        <div className='tracking-wide text-sm uppercase font-bold'>
+                    <div key={project?._id} className="mb-2">
+                      <div className="flex flex-col justify-between">
+                        <div className="tracking-wide text-sm uppercase font-bold">
                           {project?.name}
                         </div>
-                        <div className='flex items-center mb-1 font-semibold text-xs'>
-                          <span className='text-xs tracking-wider mr-1 font-extrabold text-gray-600'>
+                        <div className="flex items-center mb-1 font-semibold text-xs">
+                          <span className="text-xs tracking-wider mr-1 font-extrabold text-gray-600">
                             Link:{" "}
                           </span>
-                          <div className='relative'>
+                          <div className="relative">
                             <div>{project?.website}</div>
-                            <div className='absolute w-full left-0 bottom-0 border-t border-gray-500'></div>
+                            <div className="absolute w-full left-0 bottom-0 border-t border-gray-500"></div>
                           </div>
                         </div>
                       </div>
                       {project?.summary?.enabled && (
-                        <div className='markdown text-[13px] font-semibold text-gray-800'>
+                        <div className="markdown text-[13px] font-semibold text-gray-800">
                           <MarkdownRenderer>{project?.summary?.data}</MarkdownRenderer>
                         </div>
                       )}
@@ -253,33 +253,33 @@ export const Onyx = ({ componentRef, filter = null }) => {
           </div>
         )}
 
-        <div className='mt-5 w-full'>
+        <div className="mt-5 w-full">
           {awards?.filter((award) => award?.enabled)?.length > 0 && (
             <>
               {" "}
-              <h3 className='text-[20px] text-gray-500 uppercase font-extrabold tracking-wider'>
+              <h3 className="text-[20px] text-gray-500 uppercase font-extrabold tracking-wider">
                 Awards
               </h3>
-              <div className='border-b-2 mb-2 w-full mx-auto border-gray-300'></div>
-              <div className='mt-2'>
+              <div className="border-b-2 mb-2 w-full mx-auto border-gray-300"></div>
+              <div className="mt-2">
                 {awards?.map((award) => {
                   if (award?.enabled === true) {
                     return (
-                      <div key={award?._id} className='mb-2'>
-                        <div className='flex items-center justify-between'>
-                          <div className='text-[13.5px] uppercase tracking-wide font-bold mb-1'>
+                      <div key={award?._id} className="mb-2">
+                        <div className="flex items-center justify-between">
+                          <div className="text-[13.5px] uppercase tracking-wide font-bold mb-1">
                             {award?.name},{" "}
-                            <span className='text-xs normal-case font-base text-gray-600'>
-                              Awarded by <span className='capitalize'>{award?.awarder}</span>
+                            <span className="text-xs normal-case font-base text-gray-600">
+                              Awarded by <span className="capitalize">{award?.awarder}</span>
                             </span>
                           </div>
-                          <div className='text-xs font-bold text-gray-500'>
+                          <div className="text-xs font-bold text-gray-500">
                             {new Date(award?.date).getFullYear()}
                           </div>
                         </div>
 
                         {award?.summary?.enabled && (
-                          <div className='markdown text-xs'>
+                          <div className="markdown text-xs">
                             <MarkdownRenderer>{award?.summary?.data}</MarkdownRenderer>
                           </div>
                         )}
@@ -292,8 +292,8 @@ export const Onyx = ({ componentRef, filter = null }) => {
           )}
 
           {hobbies?.filter((hobby) => hobby?.enabled)?.length > 0 && (
-            <div className='w-full mt-5'>
-              <h3 className='text-[18px] text-gray-500 uppercase font-extrabold tracking-wide border-b-2 border-gray-300'>
+            <div className="w-full mt-5">
+              <h3 className="text-[18px] text-gray-500 uppercase font-extrabold tracking-wide border-b-2 border-gray-300">
                 Hobbies
               </h3>
 
@@ -302,7 +302,7 @@ export const Onyx = ({ componentRef, filter = null }) => {
                   return (
                     <h6
                       key={hobby?._id}
-                      className='mt-1 text-sm font-semibold text-gray-600 text-start'
+                      className="mt-1 text-sm font-semibold text-gray-600 text-start"
                     >
                       {hobby?.name}
                     </h6>
