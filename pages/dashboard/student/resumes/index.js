53c1bbd83b98b883024f7127a11c8f2ae02000f6
Thusharke <thushar.ke25@gmail.com>
2022-10-27 12:48:40 +0530

    minor changes

--- pages/dashboard/student/resumes/index.js
+++ pages/dashboard/student/resumes/index.js
@@ -333,7 +333,7 @@ const ResumeIndex = ({ user }) => {
                               <Transition
                                 as={Fragment}
                                 enter="transition ease-out duration-100"
-                                enterFrom="transform opacity-0 scale-95"
+                                enterFrom="transform opacity-0 scale-95 "
                                 enterTo="transform opacity-100 scale-100"
                                 leave="transition ease-in duration-75"
                                 leaveFrom="transform opacity-100 scale-100"
